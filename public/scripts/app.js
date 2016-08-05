700e00f972126edeb131b3571dcd15e923c9ffc5
vagrant <albert.cherng@gmail.com>
2016-08-05 00:08:21 +0000

    Add webrtc mediaStream function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,42 @@ $(() => {
     url: "/api/users"
   }).done((users) => {
     for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      // $("<div>").text(user.name).appendTo($("body"));
     }
   });;
 });
+
+
+navigator.getUserMedia ||
+(navigator.getUserMedia = navigator.mozGetUserMedia ||
+navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({
+  video: true,
+  audio: true
+  }, onSuccess, onError);
+} else {
+alert('getUserMedia is not supported in this browser.');
+}
+
+
+function onSuccess(stream) {
+var video = document.getElementById('webcam');
+var videoSource;
+if (window.webkitURL) {
+videoSource = window.webkitURL.createObjectURL(stream);
+} else {
+source = stream;
+}
+video.autoplay = true
+
+video.src = videoSource;
+
+
+
+}
+
+function onError() {
+alert('There has been a problem retreiving the streams - did you allow access?');
+}
