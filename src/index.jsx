1945e8a36470abb5378d2603aabe2f845507c59e
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-08-07 00:48:20 +0000

    Error unresolved: syntax error rendering ReactDom App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
-require("../styles/application.scss");
+require("../styles/layout.scss");
 
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import VideoPanel from './VideoPanel.jsx';
+import PhotoPanel from './PhotoPanel.jsx';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
 
