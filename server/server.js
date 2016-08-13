f009c8f11080950dd70e3508be032e1912ae0dab
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-08-13 02:30:35 +0000

    Login submit ajax request functional

--- server/server.js
+++ server/server.js
@@ -45,8 +45,6 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 
-
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -74,7 +72,6 @@ app.post("/api/signup/submit", (req, res) => {
 });
 
 app.get("/api/login/email", (req, res) => {
-  console.log(req);
   knex('users')
   .select('name', 'security_question')
   .where('email', req.query.email)
@@ -86,7 +83,7 @@ app.get("/api/login/email", (req, res) => {
 app.get("/api/login/submit", (req, res) => {
    knex('users')
     .select('id')
-    .where('answer', req.query.answer)
+    .where('security_answer', req.query.answer)
     .then((results) => {
       res.json(results);
     });
@@ -100,8 +97,6 @@ app.get("/api/login/submit", (req, res) => {
 // app.get("/list", (req, res) => {
 //   res.end(JSON.stringify(Object.keys(io.sockets.sockets)));
 // });
-
-
 
 // io.on('connection', (client) => {
 
