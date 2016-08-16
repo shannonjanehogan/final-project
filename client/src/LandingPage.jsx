ba1b4dde1e009eefc5d7ef078f5b3a97c4ff0c77
Christina Hess <mchristinahess@gmail.com>
2016-08-16 17:51:30 +0000

    Add links to landing page and update copy

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
+import {Link} from 'react-router';
 
 const Landing = React.createClass ({
   render() {
@@ -8,17 +9,25 @@ const Landing = React.createClass ({
     return (
       <div>
         <nav id="welcome-nav-bar">
-      <p className="intro"> Welcome to our app. </p>
-      <p className="description"> OUR APP is a Lighthouse Labs final project designed to encourage seniors to connect with their loved ones through intuitive and minimal user interface. </p>
+        <p className="intro"> Welcome to Gab. </p>
+      <p className="description"> Remember your favorite grandparent's living room? A place where you instantly felt at
+      home - surrounded by pictures and stories of years past. But now you live miles away and you can't go visit on
+      Sundays like before. Miles shouldn't keep you from seeing Grandma.
+                            <br/> Gab is a Lighthouse Labs final project connecting seniors with their loved ones through intuitive and minimalist user interface. </p>
+
       <p className="instruction"> To begin, choose one of the options below. </p>
     </nav>
 
     <div className="new-user">
-      <button className="button-user button-primary">I'm a new user</button>
+      <Link className="button button-user  button-primary"
+            to="/signup">I'm a new user
+          </Link>
     </div>
 
     <div className="returning-user">
-      <button className="button-user  button-primary">I'm a returning user</button>
+      <Link className="button button-user  button-primary"
+            to="/login">I'm a returning user
+          </Link>
     </div>
       </div>
 
